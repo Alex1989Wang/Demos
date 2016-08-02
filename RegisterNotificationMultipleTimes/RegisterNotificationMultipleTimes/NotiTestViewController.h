ce36e6e39d7c599f3a696f097486be6bde740086
alex_wang <alex1989wang@foxmail.com>
2016-08-02 14:14:20 +0800

    no way you can register multiple notifications

---
+++ RegisterNotificationMultipleTimes/RegisterNotificationMultipleTimes/NotiTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotiTestViewController.h
+//  RegisterNotificationMultipleTimes
+//
+//  Created by JiangWang on 8/1/16.
+//  Copyright © 2016 JiangWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotiTestViewController : UIViewController
+
+@end
