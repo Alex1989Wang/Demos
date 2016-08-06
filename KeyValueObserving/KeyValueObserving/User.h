c427cf794515372a751c4c0bded06bc01d2263cf
JiangWang <alex1989wang@foxmail.com>
2016-08-06 22:29:42 +0800

    KVO

---
+++ KeyValueObserving/KeyValueObserving/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  KeyValueObserving
+//
+//  Created by JiangWang on 16/8/3.
+//  Copyright © 2016年 JiangWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
