764673a5fec744b7c6524661d61e2c3987e769d9
alex_wang <alex1989wang@foxmail.com>
2016-08-06 19:22:09 +0800

    gift test

---
+++ emumerateUsingblock/emumerateUsingblock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  emumerateUsingblock
+//
+//  Created by JiangWang on 8/6/16.
+//  Copyright © 2016 JiangWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ emumerateUsingblock/emumerateUsingblock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  emumerateUsingblock
+//
+//  Created by JiangWang on 8/6/16.
+//  Copyright © 2016 JiangWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
