bec932df9e92cece0081fd4906e24ff821474f0d
alex_wang <alex1989wang@foxmail.com>
2016-09-18 21:06:13 +0800

    conatiner view controller

---
+++ containerViewCon/containerViewCon/UITextView+XDOverrideBaseLineMethods.h
@@ -0,0 +1,13 @@
+//
+//  UITextView+XDOverrideBaseLineMethods.h
+//  seeYouTime
+//
+//  Created by JiangWang on 9/18/16.
+//  Copyright © 2016 形点网络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (XDOverrideBaseLineMethods)
+
+@end
---
+++ containerViewCon/containerViewCon/UITextView+XDOverrideBaseLineMethods.m
@@ -0,0 +1,21 @@
+//
+//  UITextView+XDOverrideBaseLineMethods.m
+//  seeYouTime
+//
+//  Created by JiangWang on 9/18/16.
+//  Copyright © 2016 形点网络. All rights reserved.
+//
+
+#import "UITextView+XDOverrideBaseLineMethods.h"
+
+@implementation UITextView (XDOverrideBaseLineMethods)
+
+- (void)_firstBaselineOffsetFromTop {
+    
+}
+
+- (void)_baselineOffsetFromBottom {
+    
+}
+
+@end
