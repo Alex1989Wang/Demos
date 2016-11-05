a3e88d82c2662a740d90457b25f542c5e5ebc603
alex_wang <alex1989wang@foxmail.com>
2016-11-05 19:56:03 +0800

    draw and move

---
+++ MovingAndDraw/MovingAndDraw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovingAndDraw
+//
+//  Created by JiangWang on 05/11/2016.
+//  Copyright © 2016 JiangWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
