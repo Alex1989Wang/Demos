89e1ffd354b7355ede92e3fa95b0fa8c8fbcb427
JiangWang <alex1989wang@foxmail.com>
2017-07-10 10:27:56 +0800

    xcconfig files.

---
+++ DemoProjects/XCodeBuildConfiguration/XCodeBuildConfiguration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCodeBuildConfiguration
+//
+//  Created by JiangWang on 03/07/2017.
+//  Copyright © 2017 JiangWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoProjects/XCodeBuildConfiguration/XCodeBuildConfiguration/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XCodeBuildConfiguration
+//
+//  Created by JiangWang on 03/07/2017.
+//  Copyright © 2017 JiangWang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
